fe8b6598422114a74ceadff4188c51254943424f
S <30815731+slimlime@users.noreply.github.com>
2021-02-13 19:46:20 +1000

    feat(percent_indicator): show exports

--- lib/percent_indicator.dart
+++ lib/percent_indicator.dart
@@ -1,7 +1,9 @@
 library percent_indicator;
 
-export 'circle/circular_percent_indicator.dart';
-export 'linear/linear_percent_indicator.dart';
+export 'circle/circular_percent_indicator.dart' show CircularPercentIndicator;
 
-export 'circle/enums/circular_stroke_cap.dart';
-export 'circle/enums/arc_type.dart';
+export 'linear/linear_percent_indicator.dart'
+    show LinearPainter, LinearPercentIndicator, LinearStrokeCap;
+
+export 'circle/enums/circular_stroke_cap.dart' show CircularStrokeCap;
+export 'circle/enums/arc_type.dart' show ArcType;
